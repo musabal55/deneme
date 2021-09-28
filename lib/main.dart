bf29d7675878161e1100ed2a7da17f2bf152a4c3
musabal55 <musahoney@gmail.com>
2021-09-28 09:43:26 +0300

    lightBlue back

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           scaffoldBackgroundColor: Colors.lightBlue,
-          primaryColor: Colors.cyanAccent),
+          primaryColor: Colors.lightBlue),
       home: InputPage(),
     );
   }
